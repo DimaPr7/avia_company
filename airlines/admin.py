4857a30d8d93fdaa25454c35662d8d35aebfffaa
Dmytro Prokhach <dprokhach@gmail.com>
2024-07-29 15:36:28 +0300

    repaired some errors

--- airlines/admin.py
+++ airlines/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from airlines.models import Crew, Plane, Order, Client
+
+admin.site.register(Crew)
+admin.site.register(Plane)
+admin.site.register(Order)
+admin.site.register(Client)
--- avia_company/settings.py
+++ avia_company/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "avia_company.Client"
+AUTH_USER_MODEL = "airlines.Client"
