9f4cd14888f4d4dcf2f8e35b6762c9e34d0289ad
DimaPr7 <dprokhach@gmail.com>
2025-02-24 21:28:39 +0000

    some problems 8

--- avia_company/settings/development.py
+++ avia_company/settings/development.py
@@ -2,8 +2,8 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1",
-                 'avia-company.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'avia-company.onrender.com']
+
 
 DATABASES = {
     'default': {
