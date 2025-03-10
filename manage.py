83dc07d29c768d5aff7557d5210fe68dd6410ed5
DimaPr7 <dprokhach@gmail.com>
2025-03-10 22:48:36 +0000

    some problems 14

--- manage.py
+++ manage.py
@@ -6,10 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    if os.getenv('DJANGO_ENV') == 'production':
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avia_company.settings.production')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avia_company.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avia_company.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
