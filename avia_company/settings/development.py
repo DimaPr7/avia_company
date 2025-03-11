183300f93a9ddc101938dc6387244de01457d850
DimaPr7 <dprokhach@gmail.com>
2025-03-11 10:23:26 +0000

    some problems 16

--- avia_company/settings/development.py
+++ avia_company/settings/development.py
@@ -1,9 +1,7 @@
 from .base import *
 
-# Локальная разработка, DEBUG = True
 DEBUG = True
 
-# Разрешенные хосты для разработки
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DATABASES = {
--- avia_company/settings/production.py
+++ avia_company/settings/production.py
@@ -1,19 +1,17 @@
 from .base import *
 
-# Продакшн окружение, DEBUG = False
 DEBUG = False
 
-# Разрешенные хосты для продакшн
 ALLOWED_HOSTS = ['avia-company.onrender.com', '127.0.0.1', 'localhost']
 
-# Настройки базы данных для продакшн
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_DB_PORT"),
     }
 }
+
