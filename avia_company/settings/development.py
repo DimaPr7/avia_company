570bbace32de4c1ab9056f45f215f91ae85870bf
DimaPr7 <dprokhach@gmail.com>
2025-03-11 14:01:02 +0000

    solving problems 29

--- avia_company/settings/development.py
+++ avia_company/settings/development.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
